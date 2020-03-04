634e0e3850385a2502602d5702136074a5eed6d3
Márcio Alves Miguel <60950196+marciohvrum@users.noreply.github.com>
2020-03-03 22:17:21 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,11 @@
+# autor: Márcio Alves Miguel 1dsi3
+# feito em: 03/03/2020
+# instituição: ETEC DE ITANHAÉM
+
+
+x = 0 # DECLARAÇÃO DE VALOR
+valor = 2 # DECLARAÇÃO DE VARIÁVEL VALOR
+while (x <= 4):
+		valor = valor * valor
+		x = x + 1 # LAÇO DE REPETIÇÃO
+print (valor) # exibir a variável valor
